ad6eff6eb8e6d0114b13943ac0284c540eb79948
David Valverde <alu0101100296@ull.edu.es>
2020-09-01 20:07:01 +0100

    prueba para use

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const EGG = require("@ull-esit-pl-1920/p7-t3-egg-2-daviddvg7");
 
 let topEnv = EGG.topEnv;
-topEnv['hola'] = function() { print("hola");}
+topEnv['hola'] = function() { console.log("hola");}
 let specialForms = EGG.specialForms;
-specialForms['adios'] = function () {print("adios")}
+specialForms['adios'] = function () {console.log("adios")}
 
 console.log(topEnv, specialForms);
 module.exports = {topEnv, specialForms}
